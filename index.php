09db1b332a58bb5c317c75d8a3ed0122f8cc2745
hahseo <hahseo@gmail.com>
2022-06-01 13:11:26 +0530

    added index.html

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>This is the index page</title>
+</head>
+<body>
+   <h1>Hello this is the Index page</h1>
+</body>
+</html>
